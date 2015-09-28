bca987a27d54d7e440acf9aa8b0d72733c96de9b
renatolrr <renatolrr@gmail.com>
2015-09-28 19:28:16 +0200

    Modifica total.pl

--- ejemplos/total.pl
+++ ejemplos/total.pl
@@ -19,7 +19,31 @@ my $int = $bio->value('intellectual');
 my $tot=0;
 if($phy==0){
 	$tot=$tot+4;
+}
+elsif ($phy>0) {
+	$tot=$tot+1;
+}
+elsif ($phy<0) {
+	$tot=$tot+2;
 };
+if($emo==0){
+	$tot=$tot+4;
+}
+elsif ($emo>0) {
+	$tot=$tot+1;
+}
+elsif ($emo<0) {
+	$tot=$tot+2;
+};
+if($int==0){
+	$tot=$tot+4;
+}
+elsif ($int>0) {
+	$tot=$tot+1;
+}
+elsif ($int<0) {
+	$tot=$tot+1;
+};;
 
 print 'Physical:';
 print $phy, "\n";
@@ -27,5 +51,5 @@ print 'Emotional:';
 print $emo, "\n";
 print 'Intelectual:';
 print $int, "\n";
-print 'Total:',$tot ,"\n";
+print 'Total (3-12):',$tot ,"\n";
 
